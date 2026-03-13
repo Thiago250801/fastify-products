0db459038c0afd1076f52be09c3d6c0d0a7a1797
Thiago Arthur <thlimabsgim@gmail.com>
2026-03-13 15:52:34 -0400

    feat(users): add basic CRUD controller

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,28 @@
+﻿import { FastifyReply, FastifyRequest } from "fastify";
+import { UpdateUserInput, UsersService } from "../services/users.service";
+
+const service = new UsersService();
+
+export const getUsers = async (_: FastifyRequest, reply: FastifyReply) => {
+  const users = await service.findAll();
+  return reply.send(users);
+};
+
+export const getUser = async (request: FastifyRequest, reply: FastifyReply) => {
+  const { id } = request.params as { id: string };
+  const user = await service.findById(id);
+  return reply.send(user);
+};
+
+export const updateUser = async (request: FastifyRequest, reply: FastifyReply) => {
+  const { id } = request.params as { id: string };
+  const payload = request.body as UpdateUserInput;
+  const user = await service.update(id, payload);
+  return reply.send(user);
+};
+
+export const deleteUser = async (request: FastifyRequest, reply: FastifyReply) => {
+  const { id } = request.params as { id: string };
+  await service.delete(id);
+  return reply.status(204).send();
+};
