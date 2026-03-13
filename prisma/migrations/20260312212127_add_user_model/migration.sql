c57a5634117e9866baca389342c5ba91c31939fc
Thiago Arthur <thlimabsgim@gmail.com>
2026-03-13 15:49:49 -0400

    feat(db): add missing user fields to migration

--- prisma/migrations/20260312212127_add_user_model/migration.sql
+++ prisma/migrations/20260312212127_add_user_model/migration.sql
@@ -1,8 +1,10 @@
--- CreateTable
+﻿-- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'USER',
     "refreshToken" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
