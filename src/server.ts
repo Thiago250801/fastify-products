35ba81153a64a7f8df54f485a41b684b44a8b107
Thiago Arthur <thlimabsgim@gmail.com>
2026-03-13 14:28:30 -0400

    feat(server): adopt Zod type provider

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,18 @@
 import fastify from "fastify";
 import { productsRoutes } from "./routes/products.routes";
-import { Prisma } from "./generated/prisma/client";
+import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 
-const app = fastify(
-    {logger: true}
-);
-
-// tratamento de erro global
-app.setErrorHandler((error, request, reply) => {
-  // erro do Prisma: registro não encontrado
-  if (error instanceof Prisma.PrismaClientKnownRequestError) {
-
-    if (error.code === "P2025") {
-      return reply.status(404).send({
-        status: "error",
-        message: "Registro não encontrado"
-      })
-    }
-
-  }
-
-  // erro padrão
-  return reply.status(500).send({
-    status: "error",
-    message: "Erro interno do servidor"
-  })
-
-})
-
-app.register(productsRoutes);
-
-app.listen({ port: 3333 }).then(() => {
-    console.log("HTTP server running on port: " + 3333);
+const app = fastify({
+  logger: true
 });
 
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+export const server = app.withTypeProvider<ZodTypeProvider>();
+
+server.register(productsRoutes);
+
+server.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running on port: 3333");
+});
