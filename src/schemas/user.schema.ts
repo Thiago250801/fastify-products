01f4ff964d653526e01c73f0ae1fea06091d0646
Thiago Arthur <thlimabsgim@gmail.com>
2026-03-13 15:53:22 -0400

    feat(users): add user schema definitions

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,24 @@
+﻿import { z } from "zod";
+
+export const userRoleSchema = z.enum(["USER", "ADMIN"]);
+
+export const userParamsSchema = z.object({
+  id: z.uuid("ID inválido"),
+});
+
+export const userResponseSchema = z.object({
+  id: z.uuid(),
+  name: z.string(),
+  email: z.email(),
+  role: userRoleSchema,
+  createdAt: z.iso.datetime(),
+});
+
+export const userListResponseSchema = z.array(userResponseSchema);
+
+export const userUpdateSchema = z.object({
+  name: z.string().min(3).optional(),
+  email: z.email().optional(),
+  password: z.string().min(6).optional(),
+  role: userRoleSchema.optional(),
+});
