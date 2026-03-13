89b6d1ad9130e9f088ef532678e7ef2a22407cd0
Thiago Arthur <thlimabsgim@gmail.com>
2026-03-13 15:52:46 -0400

    feat(errors): add semantic HTTP error classes

---
+++ src/errors/http-errors.ts
@@ -0,0 +1,20 @@
+﻿export class NotFoundError extends Error {
+  constructor(message = "Recurso não encontrado") {
+    super(message);
+    this.name = "NotFoundError";
+  }
+}
+
+export class ForbiddenError extends Error {
+  constructor(message = "Ação proibida") {
+    super(message);
+    this.name = "ForbiddenError";
+  }
+}
+
+export class AuthenticationError extends Error {
+  constructor(message = "Não autorizado") {
+    super(message);
+    this.name = "AuthenticationError";
+  }
+}
