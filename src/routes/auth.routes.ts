90645bcab35423e24e638392d4c791d5d8a88d11
Thiago Arthur <thlimabsgim@gmail.com>
2026-03-13 14:29:18 -0400

    feat(auth): add auth routes

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from "fastify";
+import { register, login } from "../controllers/auth.controller";
+
+export async function authRoutes(app: FastifyInstance) {
+
+  app.post("/register",{} register);
+
+  app.post("/login", login);
+
+}
