bb635ba81043a79051960456ec04f6152269da21
Thiago Arthur <thlimabsgim@gmail.com>
2026-03-13 14:30:23 -0400

    fix(auth): wire auth routes to schemas

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,27 @@
-import { FastifyInstance } from "fastify";
+﻿import { FastifyInstance } from "fastify";
 import { register, login } from "../controllers/auth.controller";
+import { loginSchema, registerSchema } from "../schemas/auth.schema";
 
 export async function authRoutes(app: FastifyInstance) {
 
-  app.post("/register",{} register);
+  app.post(
+    "/register",
+    {
+      schema: {
+        body: registerSchema
+      }
+    },
+    register
+  );
 
-  app.post("/login", login);
+  app.post(
+    "/login",
+    {
+      schema: {
+        body: loginSchema
+      }
+    },
+    login
+  );
 
 }
