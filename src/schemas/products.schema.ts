053c2afc5ecf7ee83f6ce13f56817057ab6c0702
Thiago Arthur <thlimabsgim@gmail.com>
2026-03-21 16:57:19 -0400

    refactor(schemas): enhance product schemas with descriptions and examples

--- src/schemas/products.schema.ts
+++ src/schemas/products.schema.ts
@@ -2,21 +2,38 @@
 import { userResponseSchema } from "./user.schema";
 
 export const productBodySchema = z.object({
-  name: z.string().min(3, "Nome deve ter no mínimo 3 caracteres"),
-  price: z.number().positive("Preço deve ser positivo"),
+name: z
+  .string()
+  .min(3, "Nome deve ter no mínimo 3 caracteres")
+  .describe("Nome do produto").meta({
+    example: "Notebook Dell"
+  }),
+
+price: z
+  .number()
+  .positive("Preço deve ser positivo")
+  .describe("Preço do produto em reais")
+  .meta({
+    example : 3500
+  })
 });
 
 export const productParamsSchema = z.object({
-  id: z.uuid("ID inválido"),
+  id: z
+    .uuid("ID inválido")
+    .describe("ID do produto")
+    .meta({
+      example: "a3bb189e-8bf9-3888-9912-ace4e6543002"
+    })
 });
 
 const productBaseSchema = z.object({
-  id: z.uuid(),
-  name: z.string(),
-  price: z.number(),
-  userId: z.uuid(),
-  createdAt: z.iso.datetime(),
-  updatedAt: z.iso.datetime(),
+  id: z.uuid().describe("ID do produto"),
+  name: z.string().describe("Nome do produto"),
+  price: z.number().describe("Preço do produto"),
+  userId: z.uuid().describe("ID do usuário dono"),
+  createdAt: z.iso.datetime().describe("Data de criação"),
+  updatedAt: z.iso.datetime().describe("Data de atualização"),
 });
 
 export const productResponseSchema = productBaseSchema.extend({
